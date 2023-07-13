1f96d0c93deb2bf94eafd2ca767a0e3af80e5c8f
Ovidiu Cristurean <ovidiucristurean@Ovidius-MacBook-Pro.local>
2023-07-13 22:24:18 +0300

    declare colors, theme and typography for the app

---
+++ shared/src/commonMain/kotlin/com/ovidiu/multiplatformcontacts/ui/theme/Color.kt
@@ -0,0 +1,63 @@
+package com.ovidiu.multiplatformcontacts.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// LIGHT
+val PrimaryLight = Color(0xff006496)
+val OnPrimaryLight = Color(0xffffffff)
+val PrimaryContainerLight = Color(0xffcbe6ff)
+val OnPrimaryContainerLight = Color(0xff001e31)
+
+val SecondaryLight = Color(0xff006874)
+val OnSecondaryLight = Color(0xffffffff)
+val SecondaryContainerLight = Color(0xff97f0ff)
+val OnSecondaryContainerLight = Color(0xff001f24)
+
+val TertiaryLight = Color(0xff984061)
+val OnTertiaryLight = Color(0xffffffff)
+val TertiaryContainerLight = Color(0xffffd9e2)
+val OnTertiaryContainerLight = Color(0xff3e001d)
+
+val ErrorLight = Color(0xffba1a1a)
+val OnErrorLight = Color(0xffffffff)
+val ErrorContainerLight = Color(0xffffdad6)
+val OnErrorContainerLight = Color(0xff410002)
+
+val BackgroundLight = Color(0xfff8fdff)
+val OnBackgroundLight = Color(0xff001f25)
+val SurfaceLight = Color(0xfff8fdff)
+val OnSurfaceLight = Color(0xff001f25)
+val SurfaceVariantLight = Color(0xffdee3ea)
+val OnSurfaceVariantLight = Color(0xff42474d)
+
+val OutlineLight = Color(0xff72787e)
+
+// DARK
+val PrimaryDark = Color(0xff90cdff)
+val OnPrimaryDark = Color(0xff003350)
+val PrimaryContainerDark = Color(0xff004b72)
+val OnPrimaryContainerDark = Color(0xffcbe6ff)
+
+val SecondaryDark = Color(0xff4fd8eb)
+val OnSecondaryDark = Color(0xff00363d)
+val SecondaryContainerDark = Color(0xff004f58)
+val OnSecondaryContainerDark = Color(0xff97f0ff)
+
+val TertiaryDark = Color(0xffffb1c8)
+val OnTertiaryDark = Color(0xff5e1133)
+val TertiaryContainerDark = Color(0xff7b2949)
+val OnTertiaryContainerDark = Color(0xffffd9e2)
+
+val ErrorDark = Color(0xffffb4ab)
+val OnErrorDark = Color(0xff690005)
+val ErrorContainerDark = Color(0xff93000a)
+val OnErrorContainerDark = Color(0xffffdad6)
+
+val BackgroundDark = Color(0xff001f25)
+val OnBackgroundDark = Color(0xffa6eeff)
+val SurfaceDark = Color(0xff001f25)
+val OnSurfaceDark = Color(0xffa6eeff)
+val SurfaceVariantDark = Color(0xff42474d)
+val OnSurfaceVariantDark = Color(0xffc1c7ce)
+
+val OutlineDark = Color(0xff8c9198)
